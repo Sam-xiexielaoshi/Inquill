71f4be0895a11c00e76bedf7fb175dc1a6d595ff
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-06-15 02:55:27 +0530

    frontend component navbar and pages home etcetra

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import DetailPage from "./pages/DetailPage";
+// import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div data-theme="retro">
+      <button className="btn btn-outline">click me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/detail/:id" element={<DetailPage />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="bg-base-300 border-b bg-base-content/10">
+        <div className="mx-auto max-w-6xl p-4">
+            <div className="flex items-center justify-center">
+                <h1 className="text-3xl font-bold text-primary font-mono tracking-tighter">Inquill</h1>
+            </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
