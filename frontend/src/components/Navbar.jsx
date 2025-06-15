213f1bbb1d1c310486880456ad284ee9a6047bb2
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-06-15 14:50:56 +0530

    add nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { Link } from "react-router";
+import { PlusIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <header>
-      <div className="bg-base-300 border-b bg-base-content/10">
-        <div className="mx-auto max-w-6xl p-4">
-            <div className="flex items-center justify-center">
-                <h1 className="text-3xl font-bold text-primary font-mono tracking-tighter">Inquill</h1>
-            </div>
+    <header className="bg-base-300 border-b border-base-content/10">
+      <div className="mx-auto max-w-6xl p-4">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
+            Inquill
+          </h1>
+          <div className="flex items-center gap-4">
+            <Link to={"/create"} className="btn btn-primary">
+              <PlusIcon className="size-5" />
+              <span>New Note</span>
+            </Link>
+          </div>
         </div>
       </div>
     </header>
