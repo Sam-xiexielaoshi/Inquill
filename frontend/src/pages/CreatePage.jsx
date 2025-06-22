c0df92b24075393f10ef1fd80c55a9aa54a96d48
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-06-23 01:27:53 +0530

    continuation of handlesubmit function and ui developed

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,11 +1,88 @@
 import React, { useState } from "react";
-
+import { Link, Navigate } from "react-router";
+import { ArrowLeftIcon } from "lucide-react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+// This component allows users to create a new note with a title and content.
+// It includes form validation to ensure that both fields are filled out before submission.
 const CreatePage = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
-  const handleSubmit = () => {};
-  return <div>CreatePage</div>;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!title.trim() || !content.trim()) {
+      toast.error("Fields cannot be empty");
+      return;
+    }
+    if (title.length < 3 || content.length < 5) {
+      toast.error(
+        "Title must be at least 3 characters and content at least 5 characters"
+      );
+      return;
+    }
+    setLoading(true);
+    try {
+      await axios.post("http://localhost:3000/api/notes", { title, content });
+      toast.success("Note created successfully");
+      Navigate
+    } catch (error) {
+console.log(error)
+    }
+  };
+  return (
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto ">
+          <Link to={"/"} className="btn btn-ghost mb-6">
+            <ArrowLeftIcon className="size-5" />
+            Back to Notes
+          </Link>
+
+          <div className="card bg-base-200">
+            <div className="card-body">
+              <h2 className="card title text-2xl mb-4">Create a New Note</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Title</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Note Title"
+                    className="input input-bordered"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Content</span>
+                  </label>
+                  <textarea
+                    placeholder="Write your note here..."
+                    className="textarea extarea-bordered h-32"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                  />
+                </div>
+
+                <div className="card-actions justify-end">
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                    disabled={loading}
+                  >
+                    {loading ? "Creating..." : "Create Note"}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePage;
