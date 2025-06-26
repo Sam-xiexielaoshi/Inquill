2fb462d0d0768c70377df39a731ac7e18bec4912
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-06-26 15:53:52 +0530

    made notes not found page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import instance from "../lib/axios";
 import toast from "react-hot-toast";
 import Spinner from "../components/Spinner";
 import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -39,10 +40,12 @@ const HomePage = () => {
             <p className="text-primary font-medium">Loading..</p>
           </div>
         )}
+
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
         )}
