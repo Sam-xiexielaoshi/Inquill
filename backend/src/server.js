05b86265f81698680aca108c689f5dc86c58356f
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-06-26 19:31:42 +0530

    production ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,24 @@ import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //miidddle ware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
-app.use(express.json()); //parese json values
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
+app.use(express.json()); //parese json values
 app.use(rateLimiter);
 
 // app.use((req, res, next) => {
@@ -26,27 +30,16 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", noteRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 connectDB().then(() => {
   app.listen(5001, () => {
     console.log("server started in port :", PORT);
   });
 });
-
-//schizophrenia
-// Endpoint? - combo of url + http method that lets client interact with a specific resource.
-
-// app.get("/api/notes", (req, res) => {
-//   res.status(200).send("you got 5 notes");
-// });
-
-// app.post("/api/notes", (req, res) => {
-//   res.status(201).json({ message: "Note created successfully!" });
-// });
-
-// app.post("/api/notes/:id", (req, res) => {
-//   res.status(200).json({ message: "Note updated successfully!" });
-// });
-
-// app.delete("/api/notes/:id", (req, res) => {
-//   res.status(200).json({ message: "Note delete successfully!" });
-// });
